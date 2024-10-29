6b7994471958aa5882f11a9ac18dd0bae2fc02c5
pene4a74 <135062550+pene4a74@users.noreply.github.com>
2024-10-29 18:40:51 +0000

    НЕ РАБОТАЕТ ОТЛИЧНО ДАЖЕ

--- homework/КВАДРАТ В СПИСЕ.py
+++ homework/КВАДРАТ В СПИСЕ.py
@@ -1,5 +1,6 @@
 import random
 N=int(input())
+num_len=[0]
 mas = [] 
 len_cube=N-1
 for i in range(N):
@@ -14,19 +15,19 @@ if mas[0][0]==1 and mas[0][len_cube]==1 and mas[len_cube][0]==1 and mas[len_cube
     print(True)
 else:
     len_cube-=1
-    len_cube_str=len_cube
-    for i in range(N-1):
-        len_cube_str=N-1
-        print("eshkere")
-        for j in range(len_cube_str):
-            print("test1",i,j,len_cube,mas[i][j])
-            print("test2",i,j,len_cube,mas[i][j+len_cube])
-            print("test3",i,j,len_cube,mas[i+len_cube][j])
-            print("test4",i,j,len_cube,mas[i+len_cube][j+len_cube],"\n")
-            if mas[i][j]==1 and mas[i][j+len_cube]==1 and mas[i+len_cube][j]==1 and mas[i+len_cube][j+len_cube]==1 :
-                print(True)
-                break
-    i=0
-    j=0
-    len_cube_str-=1
+    for z in range(2,N):
+        print("test")
+        for i in range(z):
+            print("eshkere")
+            for j in range(z):
+                print("test1",i,j,len_cube,mas[i][j])
+                print("test2",i,j,len_cube,mas[i][j+len_cube])
+                print("test3",i,j,len_cube,mas[i+len_cube][j])
+                print("test4",i,j,len_cube,mas[i+len_cube][j+len_cube],"\n")
+                if mas[i][j]==1 and mas[i][j+len_cube]==1 and mas[i+len_cube][j]==1 and mas[i+len_cube][j+len_cube]==1 :
+                    print(True,len_cube)
+                    num_len.append(len_cube+1)
+        len_cube-=1
+print("максимальная сторона квадрата:",max(num_len))
+    
 
