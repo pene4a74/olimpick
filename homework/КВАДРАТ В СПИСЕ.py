cae5a9a0d3f2343e33c520f9bc7e198577b64a75
pene4a74 <135062550+pene4a74@users.noreply.github.com>
2024-10-30 13:26:03 +0000

    modified:   "homework/\320\232\320\222\320\220\320\224\320\240\320\220\320\242 \320\222 \320\241\320\237\320\230\320\241\320\225.py"

--- homework/КВАДРАТ В СПИСЕ.py
+++ homework/КВАДРАТ В СПИСЕ.py
@@ -9,29 +9,27 @@ for i in range(N):
         emp_mas.append(random.randint(0,1))
     mas.append(emp_mas)
 if random.randint(0,10)<=8:
-    mas[0][0]==1
+    mas[0][0]=1
+else:
+    mas[0][0]=0
 for i in range(N):
     print(mas[i])
 
-if mas[0][0]==1 and mas[0][len_cube]==1 and mas[len_cube][0]==1 and mas[len_cube][len_cube]==1 :
-    print("максимальное сторона",N)
-else:
+for z in range(1,N):
+    print("test")
+    for i in range(z):
+        print("eshkere")
+        for j in range(z):
+            print("test1",i,j,len_cube,mas[i][j])
+            print("test2",i,j,len_cube,mas[i][j+len_cube])
+            print("test3",i,j,len_cube,mas[i+len_cube][j])
+            print("test4",i,j,len_cube,mas[i+len_cube][j+len_cube],"\n")
+            if mas[i][j]==1 and mas[i][j+len_cube]==1 and mas[i+len_cube][j]==1 and mas[i+len_cube][j+len_cube]==1 :
+                print(True,len_cube)
+                num_len.append(len_cube+1)
     len_cube-=1
-    for z in range(2,N):
-        print("test")
-        for i in range(z):
-            print("eshkere")
-            for j in range(z):
-                print("test1",i,j,len_cube,mas[i][j])
-                print("test2",i,j,len_cube,mas[i][j+len_cube])
-                print("test3",i,j,len_cube,mas[i+len_cube][j])
-                print("test4",i,j,len_cube,mas[i+len_cube][j+len_cube],"\n")
-                if mas[i][j]==1 and mas[i][j+len_cube]==1 and mas[i+len_cube][j]==1 and mas[i+len_cube][j+len_cube]==1 :
-                    print(True,len_cube)
-                    num_len.append(len_cube+1)
-        len_cube-=1
-        if int(len(num_len))>0:
-            break
-print("максимальная сторона квадрата:",max(num_len))
+    if int(len(num_len))>0:
+        break
+print("максимальная площадь квадрата:",max(num_len)*max(num_len))
     
 
